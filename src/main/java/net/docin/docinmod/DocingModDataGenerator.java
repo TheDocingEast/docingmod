72884ed0ae6095f620341d461bbe49150dcb95e9
TheDocingEast <kirillporubov1@gmail.com>
2025-03-21 16:49:28 +0300

    add

---
+++ src/main/java/net/docin/docinmod/DocingModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.docin.docinmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DocingModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
